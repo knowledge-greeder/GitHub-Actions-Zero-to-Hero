99442b2fe88f13604455e4504ca2fc0415248b01
Amritanshu Narayan <81787831+knowledge-greeder@users.noreply.github.com>
2024-01-29 12:22:53 +0530

    Update2 addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,8 @@
 # app.py
-# This is a test commit
-def add(a, b, c):
-    return a + b + c
+# This is a test commit for new checks 
+def add(a, b):
+    return a + b 
 
 def test_add():
-    assert add(1, 2, 4) == 7
-    assert add(1, -1, 0) == 0
+    assert add(1, 2) == 7
+    assert add(1, -1) == 0
